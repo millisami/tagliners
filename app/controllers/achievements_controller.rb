cb5236250d1c42db85c9dbbc5ac7c7c6efaa8cf9
Millisami <millisami@gmail.com>
2013-12-06 11:10:28 +0545

    Create record in the rails app created

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -5,4 +5,13 @@ class AchievementsController < ApplicationController
   def index
     respond_with Achievement.all
   end
+
+  def create
+    respond_with Achievement.create achievement_params
+  end
+
+private
+  def achievement_params
+    params.require(:achievement).permit(:title, :achieved_at)
+  end
 end
